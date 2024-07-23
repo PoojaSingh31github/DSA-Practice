836175b74c1d9eee039b4900b1603d191a593ba9
Pooja Singh <132251143+PoojaSingh31github@users.noreply.github.com>
2024-07-23 13:39:16 +0530

    multiple choice questions quiz

--- TCAssignments/3.py
+++ TCAssignments/3.py
@@ -37,6 +37,15 @@ for i in range(len(string)):
         count+=1
 print(count)   
 
-
+import time
+timestamp = time.strftime('%H:%M:%S')
+print(timestamp)
+timestamp = time.strftime('%H')
+print(timestamp)
+timestamp = time.strftime('%M')
+print(timestamp)
+timestamp = time.strftime('%S')
+print(timestamp)
+# https://docs.python.org/3/library/time.html#time.strftime
     
 
--- TCAssignments/W3.py
+++ TCAssignments/W3.py
@@ -22,3 +22,17 @@ print(str)
 for i in range(len(string)):
     for j in range(i+1,len(string)+1):
         print(string[i:j])
+
+
+
+array=[[1,2,3,4,7], [2,3,4,5,6], [3,4,5,6,7], [4,5,6,7,8], [1,2,3,4,5]] 
+for i in range(len(array)):
+    dommyarray=[]
+    for j in range(len(array)):
+        if i+j==len(array)-1 or i==j:
+            dommyarray.append(array[i][j])
+        else:
+            dommyarray.append(" ")   
+
+    print()  
+    print(dommyarray)  
