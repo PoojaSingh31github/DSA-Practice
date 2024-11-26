7c1867dd7e4cf4ee3a67ff565d0791a9c5800fc7
PoojaSingh31github <pooja.singh.masaischool@gmail.com>
2024-11-26 22:56:04 +0530

    api key hide

--- unit5/react/firestore/firebase-config.jsx
+++ unit5/react/firestore/firebase-config.jsx
@@ -2,12 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBSZRQb08FoiumSJ-t_sx-LTB5Nm7TfCd0",
-    authDomain: "assignment-aebd2.firebaseapp.com",
-    projectId: "assignment-aebd2",
-    storageBucket: "assignment-aebd2.firebasestorage.app",
-    messagingSenderId: "480143971836",
-    appId: "1:480143971836:web:2d8e2b474e3a742f6f8e79"
+    
 };
 
 
