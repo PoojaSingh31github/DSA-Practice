dd8135b97d3c0aadeb47120ca2409c6b474d3299
Pooja Singh <132251143+PoojaSingh31github@users.noreply.github.com>
2025-05-10 15:55:35 +0530

    Create datahandling.js

---
+++ may2025/datahandling.js
@@ -0,0 +1,66 @@
+const navTree = {
+  label: "Home",
+  children: [
+    {
+      label: "Products",
+      children: [
+        {
+          label: "Electronics",
+          children: [
+              { label: "Mobile" }
+             ]
+        }
+      ]
+    }
+  ]
+};
+// console.log(navTree.children[0].children[0].children[0].label)
+
+function recusion(navTree, res=[]){
+    
+    res.push(navTree.label)
+    if(navTree.children){
+       for (let item of navTree.children){
+           console.log(item,"items")
+           recusion(item, res)
+       } 
+    }
+    return res
+    
+    
+    
+}
+console.log(recusion(navTree))
+
+
+const items = [
+  { name: "Apple", category: "Fruit" },
+  { name: "Broccoli", category: "Vegetable" },
+  { name: "Banana", category: "Fruit" },
+  { name: "Carrot", category: "Vegetable" },
+];
+
+// let obj = {};
+// for(let {name, category} of items){
+//     if(!obj[category]){
+//         obj[category] = []
+//     }
+//   obj[category].push(name)
+// }
+
+// console.log(obj)
+
+
+let res = items.reduce((acc,{name, category},index)=>{
+    (acc[category] ||= []).push(name)
+  return acc
+},{})
+
+console.log(res)
+
+
+
+
+
+
+
