5beab73d5dd19fc918297b7566bdd8ffa1c3c2bf
Pooja Singh <132251143+PoojaSingh31github@users.noreply.github.com>
2025-07-29 23:14:26 +0530

    Update object-questions.js

--- may2025/object-questions.js
+++ may2025/object-questions.js
@@ -21,6 +21,8 @@ const ans = people.reduce((acc, curr)=>{
 },{})
 console.log(ans)
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 const a = ["a", "b", "c"];
 // need this output  => { a:05, b:15, c:25 }
 let count = 1;
@@ -31,3 +33,62 @@ const reduce = a.reduce((acc, ele, ind)=>{
 },{})
 
 console.log(reduce)
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+const obj = { name: "John", age: 30 } 
+// => "name=John&age=30"
+
+let ans =[];
+let ans2 =[];
+
+for (let key in obj){
+   ans.push(key , "=" , obj[key], "&")
+//   console.log(key, "=" ,obj[key] , "&")
+}
+for (let key in obj){
+   ans2.push(`${key}=${obj[key]}`)
+}
+ans.pop()
+console.log(ans.join(""), typeof ans.join(""))
+console.log(ans2.join("&"))
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Iterate through an object and convert all keys to camelCase.
+const input = {
+  "first_name": "Alice",
+  "last-name": "Johnson",
+  "user age": 25,
+  "is_active": true
+};
+
+// {
+//   firstName: "Alice",
+//   lastName: "Johnson",
+//   userAge: 25,
+//   isActive: true
+// }
+
+const obj ={};
+for (let key in input){
+    if (key.includes("_")){
+        ans = (key.split("_"))
+    }else if(key.includes("-")){
+        ans= (key.split("-"))
+    }else if(key.includes(" ")){
+        ans = (key.split(" "))
+    }
+    let bbbb = ans.map((ele,index)=>{
+       return ele= ele.split("")[0].toUpperCase() + ele.slice(1)
+    })
+    let bbbbb= bbbb.join("")
+    obj[bbbbb] = input[key]
+    
+    // key.split("_")
+}
+console.log(obj)
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
