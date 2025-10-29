057c41680df137a20637b303394de4199f83c29f
Pooja Singh <132251143+PoojaSingh31github@users.noreply.github.com>
2025-10-29 09:42:22 +0530

    Update ExtraQuestion.js

--- may2025/ExtraQuestion.js
+++ may2025/ExtraQuestion.js
@@ -44,6 +44,163 @@ for (let i of arr){
 console.log(third , first , second)
 
 
+function twoSum(arr, target) {
+  const obj = {};
+  for (let i = 0; i < arr.length; i++) {
+    let comp = target - arr[i];
+    if (obj.hasOwnProperty(comp)) {
+      return [obj[comp], i];
+    }
+    obj[arr[i]] = i;
+  }
+}
+console.log(twoSum([2, 7, 11, 15], 9)); // [0, 1]
+
+console.log("listen".split("").sort().join("") === "silent".split("").sort().join("")); // true
+
+function rotate(nums, k) {
+  k = k % nums.length;
+  let part1 = nums.slice(0, nums.length - k);
+  let part2 = nums.slice(-k);
+  return [...part2, ...part1];
+}
+console.log(rotate([1, 2, 3, 4, 5, 6, 7], 3)); // [5,6,7,1,2,3,4]
+
+function len(s) {
+  let charSet = new Set();
+  let maxlen = 0;
+  let right = 0, left = 0;
+  while (right < s.length) {
+    if (charSet.has(s[right])) {
+      charSet.delete(s[left]);
+      left++;
+    } else {
+      charSet.add(s[right]);
+      right++;
+      maxlen = Math.max(maxlen, charSet.size);
+    }
+  }
+  return maxlen;
+}
+console.log(len("abcabcbb")); // 3
+
+function isValid(s) {
+  const st = [];
+  const map = { "(": ")", "{": "}", "[": "]" };
+  for (let i of s) {
+    if (map[i]) {
+      st.push(i);
+    } else {
+      const last = st.pop();
+      if (map[last] !== i) return false;
+    }
+  }
+  return st.length === 0;
+}
+console.log(isValid("()[]{}")); // true
+
+
+function flatten(arr, res = []) {
+  for (let i of arr) {
+    if (Array.isArray(i)) {
+      flatten(i, res);
+    } else {
+      res.push(i);
+    }
+  }
+  return res;
+}
+console.log(flatten([1, [2, [3, [4]]]])); // [1,2,3,4]
+
+
+function debounce(func, delay) {
+  let timer;
+  return function() {
+    clearTimeout(timer);
+    timer = setTimeout(() => func(), delay);
+  };
+}
+
+function throttle(func, delay) {
+  let timer = false;
+  return function() {
+    if (!timer) {
+      func();
+      timer = true;
+      setTimeout(() => (timer = false), delay);
+    }
+  };
+}
+
+ function firstUniqueChar(str){
+    const obj={};
+    for (let i of str){
+      obj[i] = obj[i] +1 || 1
+    }
+
+    for (let i in obj){
+      if (obj[i] ===1){
+        return i
+      }
+    }
+
+  }
+
+  console.log(firstUniqueChar("aaddbcdd"))
+
+  function moveZero(arr) {
+    let j = 0
+    for (let i in arr) {
+      if (arr[i] !== 0) {
+        [arr[i], arr[j]] = [arr[j], arr[i]]
+        j++
+      }
+    }
+    return arr
+  }
+
+  console.log(moveZero([0, 1, 0, 3, 12]))
+
+  function removeinplaceduplicate(arr) {
+    arr.sort()
+    let j =0
+    for (let i in arr){
+      if (arr[i] !==arr[j]){
+        arr[++j] = arr[i]
+      }
+
+    }
+    return arr.slice(0,j+1)
+    
+  }
+
+  console.log(removeinplaceduplicate([1,1,2,2,3,2,3]))
+
+  function kadane(arr){
+    let sum = -Infinity
+    let currsum =0
+    for (let i of arr){
+      currsum +=i
+      if (currsum > sum){
+        sum = currsum
+      }
+      if (currsum< 0){
+        currsum = 0
+      }
+    }
+    return sum
+  }
+
+  console.log(kadane([-2,1,-3,4,-1,2,1,-5,4]))
+
+
+
+
+
+
+
+
+
 
 
 
